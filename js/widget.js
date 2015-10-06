5100f36632135c5b1d7facfc9e087b7135e727e2
David <david.cellnigg@gmail.com>
2015-10-06 18:58:07 +0200

    added newsfeed widget

--- js/widget.js
+++ js/widget.js
@@ -40,3 +40,36 @@ function getOrdinal(n) {
 /*function weather (json) {
   aler('test');
 }*/
+
+(function($) {
+
+if (!window.google)  {
+    alert('You must include the Google AJAX Feed API script');
+    return;
+}    
+
+if (!google.feeds) google.load("feeds", "1");
+
+$.fn.gFeed = function(options) {
+    var opts = jQuery.extend({
+        target: this,
+        max:   5   // max number of items per feed
+    }, options || {});
+    
+    var g = new google.feeds.FeedControl();
+
+    this.each(function() {
+        var url = this.href || opts.url;
+        var title = opts.title || this.title || $(this).text();
+        g.addFeed(url, title);
+        g.setNumEntries(opts.max);
+    });
+    
+    $(opts.target).each(function() {
+        g.draw(this, opts.tabs ? { drawMode: google.feeds.FeedControl.DRAW_MODE_TABBED } : null );
+    });
+    
+    return this;
+};
+
+})(jQuery);
